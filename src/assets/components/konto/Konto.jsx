d34407c93c7014bc03399986dc378de851d79e75
philipkuerzel <philipkuerzel@gmail.com>
2024-01-19 15:59:18 +0100

    Änderung css

--- src/assets/components/konto/Konto.jsx
+++ src/assets/components/konto/Konto.jsx
@@ -4,7 +4,7 @@ const Konto = ({einzahlen, auszahlen, kontostand}) => {
         <h1 id="title">Banco Banana</h1>
         <div className="konto">
             <h2>Mein Girokonto</h2>
-            <div className="Kontostand">{kontostand.toFixed(2)}</div>
+            <div className="Kontostand">{kontostand.toFixed(2)} €</div>
             <input type="number" id="eingabe" placeholder="Betrag in €"/>
             <div>
                 <button className="einzahlen" onClick={einzahlen}>Einzahlen</button>
